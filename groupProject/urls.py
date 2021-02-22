eb8b92f0be64cbe4f786426e17be7cb49b78a7ca
Juan <jlizarraga@unomaha.edu>
2021-02-22 16:58:53 -0600

    Update urls.py

--- groupProject/urls.py
+++ groupProject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chineeseRestaurant/', include('chinesseRestaurant.urls', namespace='chinesseRestaurant'))
 ]
+
+
