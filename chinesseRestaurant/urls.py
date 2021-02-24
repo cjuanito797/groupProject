966d68d242102d8e220fbba5ff471e93413f2a8c
Juan <jlizarraga@unomaha.edu>
2021-02-23 21:13:41 -0600

    Update urls.py

--- chinesseRestaurant/urls.py
+++ chinesseRestaurant/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'chinesseRestaurant'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('foods/', views.ItemListView.as_view(), name='item_list')
