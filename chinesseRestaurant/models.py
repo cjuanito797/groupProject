f58f55f946d67a06a4f7ab91d65a1754c03936c5
Juan <jlizarraga@unomaha.edu>
2021-02-24 15:13:56 -0600

    Update models.py

--- chinesseRestaurant/models.py
+++ chinesseRestaurant/models.py
@@ -2,28 +2,55 @@ from django.db import models
 from django.contrib.auth.models import User
 import random
 
+from django.urls import reverse
+
 
 def create_new_ref_number():
     return str(random.randint(100000, 999999))
 
-# Create your models here.
-class Item(models.Model):
-    name = models.CharField(max_length=25)
-    id = models.CharField(max_length=6, unique=True, primary_key=True)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=140, default='Product Description')
-    objects = models.Manager()
-    image = models.ImageField(upload_to='products/%Y/%m/%d',
-                              blank=True)
-    category = models.CharField(max_length=30, default='Item Category')
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            unique=True,
+                            default='someValue')
 
     class Meta:
-        ordering = ('-name', )
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
-        """String for representing an item"""
-        return f'{self.name} ({self.id})'
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('chinesseRestaurant:item_list',
+                       args=[self.slug])
+
+
+class Item(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='items',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, default='someValue')
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('chinesseRestaurant:item_list',
+                       args=[self.id, self.slug])
 
 
 class Customer(models.Model):
