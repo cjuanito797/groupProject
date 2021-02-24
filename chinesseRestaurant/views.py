f5889af55a6c90f5ee430f98446580d696ef0d74
Juan <jlizarraga@unomaha.edu>
2021-02-24 16:16:37 -0600

    Update views.py

--- chinesseRestaurant/views.py
+++ chinesseRestaurant/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
 
 from .models import Item, Order, Category
-
-
-class ItemListView(ListView):
-    model = Item
-    queryset = Item.objects.get_queryset().order_by('name')
-    context_object_name = 'foods'
-    paginate_by = 3
-    template_name = 'items/list.html'
 
 
 def item_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    items = Item.objects.fitler(available=True)
 
+    items = Item.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         items = items.filter(category=category)
