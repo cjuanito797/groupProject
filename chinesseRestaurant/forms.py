017c094c5df7f7adedc39efa2992a5a167c57d1b
Juan <jlizarraga@unomaha.edu>
2021-02-25 21:42:37 -0600

    Update forms.py

--- chinesseRestaurant/forms.py
+++ chinesseRestaurant/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from .models import Customer
 
 class SignUp(forms.Form):
-    fName = forms.CharField(max_length=25, label='First Name')
-    lName = forms.CharField(max_length=25, label='Last Name')
-    phoneNumber = forms.CharField(max_length=10, label='Phone Number')
-    address = forms.CharField(max_length=100)
-    email = forms.EmailField()
+    fName = forms.CharField(max_length=25, label='', widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    lName = forms.CharField(max_length=25, label='', widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    phoneNumber = forms.CharField(max_length=10, label='', widget=forms.TextInput(attrs={'placeholder': 'Phone Number'}))
+    street_number = forms.CharField(max_length = 20, label='', widget=forms.TextInput(attrs={'placeholder': 'Street Number'}))
+    email = forms.EmailField(label='', widget=forms.TextInput(
+        attrs={'type': 'email',
+               'placeholder': 'E-mail address'}))
