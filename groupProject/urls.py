3061fee115d72a430f02896e378d90f41bcfb967
Juan <jlizarraga@unomaha.edu>
2021-03-04 09:20:21 -0600

    Update urls.py

--- groupProject/urls.py
+++ groupProject/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chinesseRestaurant.urls')),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
