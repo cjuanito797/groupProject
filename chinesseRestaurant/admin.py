44ad064d9bc4abb3829d075cbb11ab15860d86ad
Juan <jlizarraga@unomaha.edu>
2021-03-03 23:08:37 -0600

    Update admin.py

--- chinesseRestaurant/admin.py
+++ chinesseRestaurant/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, Customer, Order, Category
+from .models import Item, Customer, Order, Category, User
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Customer)
