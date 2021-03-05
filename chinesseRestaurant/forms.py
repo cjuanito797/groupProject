800f7f0ee4d52693540b3a3d60fe544e7dc89c54
Juan <jlizarraga@unomaha.edu>
2021-03-05 16:00:55 -0600

    Update forms.py

--- chinesseRestaurant/forms.py
+++ chinesseRestaurant/forms.py
@@ -3,14 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
+
+
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('date_of_birth', 'image', 'streetNum', 'city', 'state', 'zipcode')
+
 
 class SignUp(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='')
@@ -29,3 +33,23 @@ class SignUp(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
